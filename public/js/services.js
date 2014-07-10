136ffcc1042f814991c09ed4fe50ecf345c12c5b
unknown <lo0vin0@17649W7D.saig.frd.global>
2014-07-10 17:23:32 +1000

    Updated version and appcache.

--- public/js/services.js
+++ public/js/services.js
@@ -6,4 +6,4 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('auditApp.services', []).
-  value('version', '1.0.0');
+  value('version', '1.1.0');
