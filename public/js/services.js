a3342235b4bf4f3669fdefb24c33d7f75c5756d4
unknown <lo0vin0@17649W7D.saig.frd.global>
2014-07-14 16:44:24 +1000

    Removed deleted files from repository.

--- public/js/services.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* Services */
-
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
-angular.module('auditApp.services', []).
-  value('version', '1.1.0');
